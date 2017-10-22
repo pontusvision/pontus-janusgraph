d4ed878e63f13308767ff1634dbe0d00d3c0e1e6
unknown <lmartins@pontusnetworks.com>
2017-10-22 01:47:51 +0100

    rebuilt hbase

--- hbase/run-hbase.sh
+++ hbase/run-hbase.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export HADOOP_HOME=`pwd`
-./bin/hbase --config conf master --localRegionServers=1 start
+./bin/hbase --config conf master --localRegionServers=1 --minRegionServers=1 --masters=1 start
